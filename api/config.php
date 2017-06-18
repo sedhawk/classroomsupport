82a04e30c3f25535d660bc4243cdf9a52f9fca5b
sedhawk <speerdw@gmail.com>
2017-06-17 19:48:04 -0700

    Initialize git changes

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+class Data
+{
+	public static $host='localhost';
+	public static $port='3306';
+	public static $db='classroom_inventory';
+	public static $user='sedhawk';
+	public static $pass='finishThisSprint^';
+}
+?>
