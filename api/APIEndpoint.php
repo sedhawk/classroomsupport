77986bf10366a83284e078efff9fc08986ddc5ec
Ryan Middleton <ryanmid@gmail.com>
2017-06-18 11:27:00 -0700

    Code Cleanup
    Cleaned up the code, added comments, and refactored to handle edge cases, prevent exceptions, and use the new HTTP utility class

--- api/APIEndpoint.php
+++ api/APIEndpoint.php
@@ -1,5 +1,14 @@
 <?php
+require_once('HTTPUtils.php');
 
+/**
+ * Class APIEndpoint
+ *
+ * This class creates an object that serves as a single endpoint for a RESTFUL API, for performing basic CRUD (Create,
+ * READ, UPDATE, and DELETE) operations on a single database table. Given the name of a database table, the created
+ * endpoint object will perform these operation using JSON to receive request data and to send response data. The
+ * object will also assume that the table column names exactly match the JSON property names.
+ */
 class APIEndpoint
 {
 	protected $host = '';
@@ -9,8 +18,18 @@ class APIEndpoint
 	protected $table = '';
 	protected $pdo = NULL;
 
-	function __construct($host, $port, $db, $user, $pass, $table)
+	/**
+	 * APIEndpoint constructor.
+	 * @param $host - The host name or IP for the database server
+	 * @param $port - The port on which the database server is connected
+	 * @param $db - The name of the database to which to connect
+	 * @param $user - The username of the database account to use
+	 * @param $pass - The password for the database user account
+	 * @param $table - The name of the table on which this API endpoint should perform CRUD operations
+	 */
+	public function __construct($host, $port, $db, $user, $pass, $table)
 	{
+		// Save the endpoint database information
 		$this->host = $host;
 		$this->port = $port;
 		$this->db   = $db;
@@ -18,10 +37,14 @@ class APIEndpoint
 		$this->pass = $pass;
 		$this->table = $table;
 
+		// Connect the endpoint to the database
 		$this->connect();
 	}
 
-	function connect()
+	/**
+	 * Create a PDO connection to the database specified in the constructor
+	 */
+	protected function connect()
 	{
 		try
 		{
@@ -33,7 +56,12 @@ class APIEndpoint
 		}
 	}
 
-	function isValidJSON($data) 
+	/**
+	 * Returns true if the passed in data is a valid JSON string, false otherwise
+	 * @param $data - The data to validate as a JSON string
+	 * @return bool - True/False indicating if the $data is valid or not
+	 */
+	protected function isValidJSON($data)
 	{
 		if(strlen($data) <= 0)
 		{
@@ -46,50 +74,70 @@ class APIEndpoint
 		}
 	}
 
-	function createRecords($data)
+	/**
+	 * Insert the given JSON records into the db table associated with this endpoint
+	 * @param $data
+	 */
+	protected function createRecords($data)
 	{
+		// Only work with valid JSON data
 		if($this->isValidJSON($data))
 		{
-			$query = "INSERT INTO $this->table (";
-
+			// Build a list of column names from the given JSON data
+			$columnString = '';
 			$columns = json_decode($data, true);
 			foreach($columns as $columnName => $column)
 			{
-				$query .= "$columnName,";	
+				$columnString .= "$columnName,";
 			}
-			$query = rtrim($query, ',');
+			$query = rtrim($columnString, ',');
 
-			$query .= ") VALUES (";
+			// Build a list of column values from the given JSON data
+			$valueString = '';
 			foreach($columns as $column)
 			{
-				$query .= "'$column',";
+				$valueString .= "'$column',";
 			}
-			$query = rtrim($query, ',');
-			$query .= ")";
+			$valueString = rtrim($query, ',');
 
+			// Run a query to insert the columns into the database
+			$query = "INSERT INTO ? ( ? ) VALUES ( ? )";
 			$statement = $this->pdo->prepare($query);
-			$result = $statement->execute();
+			$result = $statement->execute([$columnString, $valueString]);
 
+			// If the query was successful then return the created row in JSON encoding
 			if($result)
 			{
 				$columns["$this->table" . "ID"] = $this->pdo->lastInsertId();
 				print json_encode($columns);
 			}
+
+			// Otherwise return an error response
 			else
 			{
-				header("HTTP/1.1 500 Internal Server Error");
-				print "There was an error processing this request";
+				HTTPUtils::sendResponse(500, "{ \"error\": \"There was an error fulfilling this request\" }");
 			}
 		}
 		else
 		{
-			print("invalid json");
+			HTTPUtils::sendResponse(400, "{ \"error\": \"The request did not contain valid JSON\" }");
 		}
 	}
-	
-	function getRecords($data)
+
+	/**
+	 * Return all of the rows in the table associated with this endpoint, or a single one specified by primary key
+	 * @param $data - The request data for this request
+	 */
+	protected function getRecords($data)
 	{
-		$id = $data['id'];
+		if (isset($data['id']))
+		{
+			$id = $data['id'];
+		}
+		else
+		{
+			$id = null;
+		}
 
 		$query = "SELECT * FROM $this->table";
 		if($id) 
@@ -102,11 +150,18 @@ class APIEndpoint
 		print $resultJSON;		
 	}
 
-	function updateRecords($data)
+	/**
+	 * Update a single row in the table associated with this endpoint
+	 * @param $data - The data with which to update the record
+	 */
+	protected function updateRecords($data)
 	{
 	}
 
-	function handleRequest()
+	/**
+	 * Handle the current server request
+	 */
+	public function handleRequest()
 	{
 		// Create
 		if($_SERVER['REQUEST_METHOD'] === 'POST')
@@ -131,6 +186,12 @@ class APIEndpoint
 		{
 			$this->deleteRecord($_REQUEST);	
 		}
+
+		else
+		{
+			$statusCode = $_SERVER['REQUEST_METHOD'];
+			HTTPUtils::sendResponse(504, "The received status code ($statusCode) is unsupported or not a valid HTTP status code");
+		}
 	}
 }
-?>
+
--- api/crud.php
+++ api/crud.php
@@ -1,6 +1,7 @@
 <?php
-require 'APIEndpoint.php';
-require 'config.php';
+require_once('config.php');
+require_once('APIEndpoint.php');
+require_once('HTTPUtils.php');
 
 $tables = ['person', 'campus', 'images', 'room_type', 'building', 'room', 'department', 'department_person', 'device_type', 'vendor', 'collaborator', 'remediation', 'notes', 'program', 'device', 'vendor_person', 'checklist', 'collab_remed', 'history_table', 'checklist_item', 'deploy_device', 'install_program'];
 
@@ -14,14 +15,12 @@ foreach($tables as $table)
 
 if(isset($_REQUEST['dataset']))
 {
-	http_response_code(200);
-	header('Content-Type: application/json');
 	$endpoints[$_REQUEST['dataset']]->handleRequest();
 }
 else
 {
 	http_response_code(404);
 	header('Content-Type: text/html');
+
     print("<p>This is not the page you are looking for</p>");
 }
-?>
