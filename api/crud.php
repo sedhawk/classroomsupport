52ddddc8690420feeeb4ec7d6e1c41843758832f
Unknown <speerdw@gmail.com>
2018-06-17 21:49:30 -0700

    Added Frontend React Components

--- api/crud.php
+++ api/crud.php
@@ -3,7 +3,6 @@ require 'APIEndpoint.php';
 require 'config.php';
 
 $tables = ['person', 'campus', 'images', 'room_type', 'building', 'room', 'department', 'department_person', 'device_type', 'vendor', 'collaborator', 'remediation', 'notes', 'program', 'device', 'vendor_person', 'checklist', 'collab_remed', 'history_table', 'checklist_item', 'deploy_device', 'install_program'];
-
 $endpoints = [];
 foreach($tables as $table)
 {
@@ -12,16 +11,15 @@ foreach($tables as $table)
 
 //---------------
 
-if(isset($_REQUEST['dataset']))
+header('Content-Type: application/json');
+if(isset($_REQUEST['dataset']) && isset($endpoints[$_REQUEST['dataset']]))
 {
 	http_response_code(200);
-	header('Content-Type: application/json');
 	$endpoints[$_REQUEST['dataset']]->handleRequest();
 }
 else
 {
 	http_response_code(404);
-	header('Content-Type: text/html');
-    print("<p>This is not the page you are looking for</p>");
+    print("{ \"error\": \"This is not the data you are looking for\"}");
 }
 ?>
